c48acb8e734809dd5b54c7f4b90bc9c5fab8719a
Bakudankun <bakudankun@gmail.com>
2016-04-03 22:53:26 +0900

    Fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$1" == npm ]; then
 
 	if [ -n "$DB_PORT_27017_TCP_ADDR" ]; then
 		if [ -n "$MONGO_URI" ]; then
-			echo >&2 'warging: both DB_PORT_27017_TCP_ADDR and MONGO_URI found'
+			echo >&2 'warning: both DB_PORT_27017_TCP_ADDR and MONGO_URI found'
 			echo >&2 "  Connectiong to MONGO_URI ($MONGO_URI)"
 			echo >&2 '  instead of linked MongoDB conatiner'
 		fi
@@ -17,7 +17,7 @@ if [ "$1" == npm ]; then
 
 	if [ -n "$REDIS_PORT_6379_TCP_ADDR" ]; then
 		if [ -n "$REDIS_URL" ]; then
-			echo >&2 'warging: both REDIS_PORT_6379_TCP_ADDR and REDIS_URL found'
+			echo >&2 'warning: both REDIS_PORT_6379_TCP_ADDR and REDIS_URL found'
 			echo >&2 "  Connectiong to REDIS_URL ($REDIS_URL)"
 			echo >&2 '  instead of linked Redis conatiner'
 		else
